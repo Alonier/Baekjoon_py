701346917acd15d5ef838cebe5fe2e328f8aee1a
junheeLee <junhee2727@gmail.com>
2025-07-24 11:32:37 +0900

    25.07.22

---
+++ sprout/9498.py
@@ -0,0 +1,14 @@
+a = int(input())
+if(a >= 60):
+    if(a >= 70):
+        if(a>= 80):
+            if(a>=90):
+                print('A')
+            else:
+                print('B')
+        else:
+            print('C')
+    else:
+        print('D')
+else:
+    print('F')
